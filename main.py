80f4d1aac0adfd28e1af5e910f123b40081f11ab
Adpros7 <pranit.advik@gmail.com>
2026-08-20 09:38:38 -0400

    wrap main into if name = main

--- main.py
+++ main.py
@@ -61,57 +61,61 @@ def log_move(x, y):
 
 keyListen: Listener = KeyboardListener(on_press=log_press, on_release=log_release)
 mouseListen = MouseListener(on_click=log_click, on_scroll=log_scroll, on_move=log_move)
+
 def run():
     keyListen.start()
     mouseListen.start()
+    while (
+        len([
+            event
+            for event in events[-20:]
+            if event.type == "Key" and event.info["key"] == "esc"
+        ])
+        != 10
+    ):
+        pass
 
-while (
-    len([
-        event
-        for event in events[-20:]
-        if event.type == "Key" and event.info["key"] == "esc"
-    ])
-    != 10
-):
-    pass
-
-keyListen.stop()
-mouseListen.stop()
-
-print(events)
-
-template = """from time import sleep
-from pynput.mouse import Controller as MouseController, Button
-from pynput.keyboard import Controller as KeyboardController
-
-mouse = MouseController()
-keyboard = KeyboardController()
+if __name__ == "__main__":
+    run()
 
 
-"""
+    keyListen.stop()
+    mouseListen.stop()
 
-events = events[:-20]
+    print(events)
 
-for i in events:
-    if i.type == "Time":
-        template += f"sleep({i.time})"
+    template = """from time import sleep
+    from pynput.mouse import Controller as MouseController, Button
+    from pynput.keyboard import Controller as KeyboardController
 
-    if i.type == "Key":
-        template += f"keyboard.{'press' if i.info['pressStatus'] == 'down' else 'release'}('{i.info['key']}')"
+    mouse = MouseController()
+    keyboard = KeyboardController()
 
-    if i.type == "MouseButton":
-        template += (
-            f"mouse.{'press' if i.info['pressed'] else 'release'}({i.info['button']})"
-        )
 
-    if i.type == "MouseMove":
-        template += f"mouse.position = ({i.info['x'], i.info['y']})"
+    """
 
-    if i.type == "MouseScroll":
-        template += f"mouse.position = ({i.info['x'], i.info['y']})\n"
-        template += f"mouse.scroll({i.info['dx']}, {i.info['dy']})"
+    events = events[:-20]
 
-    template += "\n"
+    for i in events:
+        if i.type == "Time":
+            template += f"sleep({i.time})"
 
-print(template)
-copy(template)
+        if i.type == "Key":
+            template += f"keyboard.{'press' if i.info['pressStatus'] == 'down' else 'release'}('{i.info['key']}')"
+
+        if i.type == "MouseButton":
+            template += (
+                f"mouse.{'press' if i.info['pressed'] else 'release'}({i.info['button']})"
+            )
+
+        if i.type == "MouseMove":
+            template += f"mouse.position = ({i.info['x'], i.info['y']})"
+
+        if i.type == "MouseScroll":
+            template += f"mouse.position = ({i.info['x'], i.info['y']})\n"
+            template += f"mouse.scroll({i.info['dx']}, {i.info['dy']})"
+
+        template += "\n"
+
+    print(template)
+    copy(template)
