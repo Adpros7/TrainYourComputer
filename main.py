246d118dbf120dd7601c2534860ea48c740f6a0a
Adpros7 <pranit.advik@gmail.com>
2026-08-19 23:33:15 -0400

    switch to using the new Event class

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 from pathlib import Path
 from pydantic import BaseModel
 from pynput.keyboard._base import Listener
-from typing import Literal
+from typing import Literal, Optional
 from pynput.keyboard import Listener as KeyboardListener
 from pynput.mouse import Listener as MouseListener
 from stopwatch import Stopwatch
 
-events: list = []
 watch = Stopwatch()
 watch.start()
 
 
 class EventType(BaseModel):
-    type: Literal["Key", "MouseButton", "MouseScroll", "MouseMove"]
+    type: Literal["Key", "MouseButton", "MouseScroll", "MouseMove", "Time"]
 
-def Log(event: dict, type: EventType):
-    events.append(watch.duration)
+class Event(BaseModel):
+    type: Literal["Key", "MouseButton", "MouseScroll", "MouseMove", "Time"]
+    info: dict = {}
+    time: Optional[int | float] = None
+
+events: list[Event] = []
+
+def Log(event: Event):
+    events.append(Event(type="Time", time=watch.duration))
     watch.restart()
-    events.append((event, type))
+    events.append(event)
 
 
 def log_press(event):
     construct = {"key": str(event).split(":")[0].replace("'", "").replace("Key.", ""), "pressStatus": "up"}
-    Log(construct, EventType(type="Key"))
+    Log(Event(type="Key", info=construct))
 
 def log_release(event):
     construct = {"key": str(event).split(":")[0].replace("'", "").replace("Key.", ""), "pressStatus": "down"}
-    Log(construct, EventType(type="Key"))
+    Log(Event(type="Key", info=construct))
 
 def log_click(x, y, button, pressed):
     args = locals()
-    print(args)
     args["button"] = str(args["button"]).replace("Button.", "")
-    Log(args, EventType(type="MouseButton"))
+    Log(Event(info=args, type="MouseButton"))
 
 def log_scroll(x, y, dx, dy):
     args = locals()
-    Log(args, EventType(type="MouseScroll"))
+    Log(Event(info=args, type="MouseScroll"))
 
 def log_move(x, y):
     args = locals()
-    Log(args, EventType(type="MouseMove"))
+    Log(Event(info=args, type="MouseMove"))
 
 keyListen: Listener = KeyboardListener(on_press=log_press, on_release=log_release)
 keyListen.start()
@@ -64,3 +69,5 @@ keyboard = KeyboardController()
 
 """
 
+for i in events:
+    if i[1].type == 
