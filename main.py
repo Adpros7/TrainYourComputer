759167d8bb424d1992db1c186d5c01501a6ffb7b
Adpros7 <pranit.advik@gmail.com>
2026-08-19 21:55:18 -0400

    keyboard only and doesnt really do anything except log time. between presses

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from trainyourcomputer!")
+from typing import Literal
+from pynput.keyboard import Listener
+from stopwatch import Stopwatch
+
+events: list[tuple | float] = []
+watch = Stopwatch()
+watch.start()
 
 
-if __name__ == "__main__":
-    main()
+def keyLog(event, type: Literal["up", "down"]):
+    print(events)
+    events.append(watch.duration)
+    watch.restart()
+    events.append((str(event).split(":")[0].replace("'", ""), type))
+
+def log_press(event):
+    keyLog(event, "down")
+
+def log_release(event):
+     keyLog(event, "up")
+
+
+keyListen = Listener(on_press=log_press, on_release=log_release)
+keyListen.start()
+keyListen.join()
